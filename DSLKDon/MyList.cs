754b06c6c9696afefb635d02e01667cd4403e825
Namhahaha1110 <23dh114467@st.huflit.edu.vn>
2024-10-13 15:49:30 +0700

    danh sách liên kết p2

---
+++ DSLKDon/MyList.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSLKDon
+{
+    internal class MyList
+    {
+        private IntNode first;
+        private IntNode last;
+
+        internal IntNode First { get => first; set => first = value; }
+        internal IntNode Last { get => last; set => last = value; }
+        public MyList()
+        {
+            first = null;
+            last = null;
+        }
+        public bool IsEmty()
+        {
+            return first == null;
+
+        }
+        public void AddFirst(IntNode newNode)
+        {
+            if (IsEmty())
+                first = last = newNode;
+            else
+            {
+                newNode.Next = first;
+                first = newNode;
+            }
+        }
+        //AddLast
+        public void AddLast(IntNode newNode)
+        {
+            if (IsEmty())
+                first = last = newNode;
+            else
+            {
+                last.Next = newNode;
+                last = newNode;
+            }
+        }
+        public IntNode GetMax()
+        {
+            if (IsEmty()) return null;
+            IntNode maxNode = first;
+            for (IntNode p = first; p != null; p = p.Next)
+            {
+                if (p.Data > maxNode.Data)
+                    maxNode = p;
+            }
+            return maxNode;
+        }
+        public IntNode GetMin()
+        {
+            if (IsEmty()) return null;
+            IntNode minNode = first;
+            for (IntNode p = first; p != null; p = p.Next)
+            {
+                if (p.Data < minNode.Data)
+                    minNode = p;
+            }
+            return minNode;
+        }
+        public MyList GetEvenList()
+        {
+            MyList evenList = new MyList();
+            IntNode p = first;
+            while (p != null)
+            {
+                if (p.Data % 2 == 0)
+                {
+                    evenList.AddLast(new IntNode(p.Data));
+                }
+                p = p.Next;
+            }
+            return evenList;
+        }
+        public MyList GetOddList()
+        {
+            MyList oddList = new MyList();
+            IntNode p = first;
+            while (p != null)
+            {
+                if (p.Data % 2 != 0)
+                {
+                    oddList.AddLast(new IntNode(p.Data));
+                }
+                p = p.Next;
+            }
+            return oddList;
+        }
+        public MyList JoinList1(MyList myList2)
+        {
+            MyList myList3=new MyList();
+            IntNode p = first;
+            while (p != null)
+            {
+                IntNode newNode=new IntNode(p.Data);
+                myList3.AddLast(newNode);
+                p= p.Next;
+            }
+            p = myList2.first;
+            while(p != null)
+            {
+                IntNode newNode=new IntNode(p.Data);
+                myList3.AddLast(newNode);
+                p = p.Next;
+            }
+            return myList3;
+        }
+        public MyList JoinList2(MyList myList2)
+        {
+            MyList myList3 = new MyList();
+            IntNode p1=first;
+            IntNode p2 = myList2.first;
+            while(p1!= null && p2!=null)
+            {
+                IntNode newNode=new IntNode(p1.Data);
+                myList3.AddLast(newNode);
+                p1 = p1.Next;
+                newNode=new IntNode(p2.Data);
+                myList3.AddLast(newNode);
+                p2 = p2.Next;
+            }
+            while(p1!= null)
+            {
+                IntNode newNode = new IntNode(p1.Data);
+                myList3.AddLast(newNode);
+                p1= p1.Next;
+            }
+            while(p2!= null)
+            {
+                IntNode newNode= new IntNode(p2.Data);  
+                myList3.AddLast(newNode);
+                p2= p2.Next;
+            }
+            return myList3;
+        }
+
+
+       
+        public void Input1()
+        {
+            int x;
+            do
+            {
+                Console.Write("Mời nhập giá trị (Nhập giá trị 0 để kết thúc): ");
+                int.TryParse(Console.ReadLine(), out x);
+                if (x == 0)
+                    return;
+                IntNode newNode = new IntNode(x);
+                AddFirst(newNode);
+            } while (true);
+
+        }
+        public void Input2()
+        {
+            int x;
+            do
+            {
+                Console.Write("Mời nhập giá trị (Nhập giá trị 0 để kết thúc): ");
+                int.TryParse(Console.ReadLine(), out x);
+                if (x == 0)
+                    return;
+                IntNode newNode = new IntNode(x);
+                AddLast(newNode);
+            } while (true);
+
+        }
+        public void ShowList()
+        {
+            IntNode p = first;
+            while (p != null)
+            {
+                Console.Write($"{p.Data} -> ");
+                p = p.Next;
+            }
+            Console.WriteLine("null");
+        }
+        public IntNode SearchX(int x)
+        {
+            IntNode p = first;
+            while (p != null)
+            {
+                if (p.Data == x)
+                    return p;
+                p = p.Next;
+            }
+            return null;
+        }
+
+    }
+}
+
---
+++ DSLKDon/Program.cs
@@ -0,0 +1,125 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DSLKDon;
+using System.Collections.Generic;
+
+static void TextInput()
+{
+    MyList list = new MyList();
+    list.Input();
+    Console.WriteLine("DSLK số nguyên: ");
+    list.ShowList();
+   
+  
+    int x = int.Parse(Console.ReadLine());
+    IntNode kq = list.SearchX(x);
+    if (kq == null)
+        Console.WriteLine($"Tìm không thấy {x}");
+    else
+        Console.WriteLine($"Tìm thấy {x}");
+}
+static MyList TestInput1()
+{
+    MyList list1=new MyList();
+    list1.Input1();
+    Console.Write("DSLK số nguyên AddFrist: ");
+    list1.ShowList();
+    return list1;
+}
+static void TestInput2()
+{
+    MyList list1=TestInput1();
+    MyList list2 = new MyList();
+    list2.Input2();
+}
+static int Menu()
+{
+    Console.WriteLine("/t/MENU");
+    Console.WriteLine("1. Thêm một node có giá trị x vào đầu danh sách");
+    Console.WriteLine("2. Thêm một node có giá trị x vào cuối danh sách");
+    Console.WriteLine("3. Nhập các giá trị thêm vào đầu danh sách");
+    Console.WriteLine("4. Nhập các giá trị thêm vào cuối danh sách");
+    Console.WriteLine("5. Hiển thị giá trị của danh sách ra màn hình");
+    Console.WriteLine("6. Tìm và trả về node có giá trị x (nếu có)");
+    Console.WriteLine("7. Trả về node có giá trị lớn nhất");
+    Console.WriteLine("8. Trả về node có giá trị nhỏ nhất");
+    Console.WriteLine("9. Trả về danh sách chỉ chứa số chẵn");
+    Console.WriteLine("10. Trả về danh sách chỉ chứa số lẻ");
+    Console.WriteLine("11. Tạo danh sách list2 vào cuối danh sách list1");
+    Console.WriteLine("12. Tạo danh sách nối 2 danh sách chứa số chẵn và số lẻ đan xen");
+    Console.WriteLine("0. Thoát");
+    while (true)
+    {
+        Console.Write("Mời bạn nhập chức năng (1 - 12): ");
+        int chon;
+        if (int.TryParse(Console.ReadLine(), out chon){
+            if(chon>=0&&chon<=12) return chon;
+            Console.WriteLine("Bạn nhập sai, mời nhập lại");
+        }
+        Console.WriteLine("Bạn phải nhập số nguyên");
+    }
+}
+static void XuLy(MyList objList, int chon)
+{
+    MyList evenList=new MyList(), oddList=new MyList();
+    switch (chon)
+    {
+        case 1: Console.Write("Mời nhập giá trị x cho node mới: ");
+            int x=int.Parse(Console.ReadLine());
+            IntNode newNode = new IntNode(x);
+            objList.AddFirst(newNode);
+            break;
+        case 2: Console.Write("Mời nhập giá trị x cho node mới: ");
+            x = int.Parse(Console.ReadLine());
+           newNode=new IntNode(x);
+            objList.AddLast(newNode);
+            break ;
+        case 3: objList.Input1();
+            break;
+        case 4: objList.Input2();
+            break;
+        case 5: objList.ShowList();
+            break;
+        case 6: Console.Write("Mời nhập giá trị x cần tìm: ");
+            x=int.Parse(Console.ReadLine());
+            IntNode kq = objList.SearchX(x);
+            if (kq == null)
+            {
+                Console.WriteLine($"Tìm không thấy {x}");
+            }
+            else Console.WriteLine($"Tìm thấy {x}");
+            break;
+        case 7:
+            IntNode maxNode = objList.GetMax();
+            Console.WriteLine($"Giá trị lớn nhất trong danh sách là {maxNode.Data}");
+            break;
+        case 8:
+            IntNode minNode = objList.GetMin();
+            Console.WriteLine($"Giá trị nhỏ nhất trong danh sách là {minNode.Data}");
+            break;
+        case 9:
+            MyList evenlist = objList.GetEvenList();
+            Console.WriteLine("Danh sách số chẵn: ");
+            evenlist.ShowList();
+            break ;
+        case 10:
+            MyList oddlist = objList.GetOddList();
+            Console.WriteLine("Danh sách số lẻ: ");
+            oddlist.ShowList();
+            break;
+        case 11: MyList list3=evenList.JoinList1(oddList);
+            Console.WriteLine("DSLK số nguyên sau khi Join1: ");
+            list3.ShowList();
+            break;
+        case 12:
+            MyList listKq = evenList.JoinList1(oddList);
+            Console.WriteLine("DSLK số nguyên sau khi Join2: ");
+            listKq.ShowList();
+            break;
+
+    }
+}
+
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+static void
+TextInput();
